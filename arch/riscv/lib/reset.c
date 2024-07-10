c19ce1b3b958898bf4ffe3f99bbbd2c75e48d5c1
xuxiang <xuxiang@eswincomputing.com>
2024-07-10 11:06:05 +0800

    feat:Eswin Uboot Board level adaptation
    Changelogs:
        1. Adapt to Eswin eic7700 chip
        2. Support Eswin EIC7700 EVB Board, SIFIVE hifive_premier_550 Board
        3. Support HDMI, USB, EMMC, SD,SATA,GPIO,MAILBOX,PINCTRL,I2C,SPI,MIPI
        4. Support write eic7700 otp, eic7700 burn and update firmware
        5. add config: CONFIG_CONSOLE_NORMAL=y, CONFIG_VIDEO_ANSI=y, CONFIG_USB_KEYBOARD=y, CONFIG_USE_PREBOOT=y; PREBOOT add "usb start"
        6. es_burn add download/write progress bar
        7. fit(HDMI):Code compliance check
    
    Signed-off-by: Xiang Xu <xuxiang@eswincomputing.com>

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -5,10 +5,13 @@
 
 #include <command.h>
 #include <cpu_func.h>
+#include <asm/io.h>
 
+#define RESET_REG	(0x51828300UL)
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	reset_cpu();
+	writel(0x1AC0FFE6, RESET_REG);
 
 	return 0;
 }
