cde124a390f12060d4d1e00f5cddc91b0c71c4c7
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-08-02 18:45:12 +0200

    riscv: add conversion in do_reset() function
    Avoid an error
    
        arch/riscv/lib/reset.c: In function ‘do_reset’:
        arch/riscv/lib/reset.c:10:25: error: passing argument 2 of ‘writel’
        makes pointer from integer without a cast [-Wint-conversion]
           10 | #define RESET_REG       (0x51828300UL)
              |                         ^~~~~~~~~~~~~~
              |                         |
              |                         long unsigned int
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -7,7 +7,7 @@
 #include <cpu_func.h>
 #include <asm/io.h>
 
-#define RESET_REG	(0x51828300UL)
+#define RESET_REG	((void *)0x51828300UL)
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	reset_cpu();
