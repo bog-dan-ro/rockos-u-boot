3a5707ca83efc57d17aa3dd288913088e82a3d8c
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2024-08-11 10:10:02 +0200

    riscv/eswin: use sysreset driver
    The standard way of implementing poweroff and reset is a sysreset driver.
    
    As OpenSBI already implements power off and reboot we can use the SBI
    sysreset driver instead of adding an ESWIN specific sysreset driver in
    U-Boot.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- arch/riscv/lib/reset.c
+++ arch/riscv/lib/reset.c
@@ -5,13 +5,11 @@
 
 #include <command.h>
 #include <cpu_func.h>
-#include <asm/io.h>
 
-#define RESET_REG	((void *)0x51828300UL)
 int do_reset(struct cmd_tbl *cmdtp, int flag, int argc, char *const argv[])
 {
 	reset_cpu();
-	writel(0x1AC0FFE6, RESET_REG);
+	printf("reset not supported yet\n");
 
 	return 0;
 }
