c98e6e4c41511f07111a8cffea177525ff633651
Xiang Xu <xuxiang@eswincomputing.com>
2024-10-09 15:20:22 +0800

    fix:bootspi write err
    Changelogs:
    1. bootspi write err
    2. fdtfile set eswin/eic7702-evb-a1.dtb in file eic7702.c
    
    Signed-off-by: Xiang Xu <xuxiang@eswincomputing.com>

--- env/sf.c
+++ env/sf.c
@@ -239,7 +239,7 @@ static int env_sf_save(void)
 
 	sector = DIV_ROUND_UP(CONFIG_ENV_SIZE, sect_size);
 	#if defined(CONFIG_ESWIN_SPI)
-	es_flash_region_wp_cfg((void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 0);
+	es_flash_region_wp_cfg(env_flash, (void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 0);
 	#endif
 	puts("Erasing SPI flash...");
 	ret = spi_flash_erase(env_flash, CONFIG_ENV_OFFSET,
@@ -260,7 +260,7 @@ static int env_sf_save(void)
 			goto done;
 	}
 	#if defined(CONFIG_ESWIN_SPI)
-	es_flash_region_wp_cfg((void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 1);
+	es_flash_region_wp_cfg(env_flash, (void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 1);
 	#endif
 	ret = 0;
 	puts("done\n");
@@ -320,7 +320,7 @@ static int env_sf_erase(void)
 	if (ret)
 		return ret;
 	#if defined(CONFIG_ESWIN_SPI)
-	es_flash_region_wp_cfg((void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 0);
+	es_flash_region_wp_cfg(env_flash, (void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 0);
 	#endif
 
 	memset(&env, 0, sizeof(env_t));
@@ -333,7 +333,7 @@ static int env_sf_erase(void)
 
 done:
 	#if defined(CONFIG_ESWIN_SPI)
-	es_flash_region_wp_cfg((void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 1);
+	es_flash_region_wp_cfg(env_flash, (void *)CONFIG_ENV_OFFSET, CONFIG_ENV_SIZE, 1);
 	#endif
 	spi_flash_free(env_flash);
 
